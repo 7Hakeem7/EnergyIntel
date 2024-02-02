78c0be90e221ad532c55791672c8795c1bc40121
7Hakeem7 <7hakeemsyed7@gmail.com>
2024-02-02 16:58:05 +0530

    commit 3

--- app.py
+++ app.py
@@ -108,3 +108,10 @@ def inspect_data():
 #   output = round(model.predict (X) [0], 3)
 #   return render_template('output.html',output=output)
 
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
